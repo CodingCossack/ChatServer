538940ca6b1134a27920975aa016439359434457
Dima <dimastasy@protonmail.com>
2023-06-12 15:32:30 +0100

    Client side base

--- src/com/codingcossack/chatserver/Client.java
+++ src/com/codingcossack/chatserver/Client.java
@@ -1,4 +1,50 @@
+// Package declaration to organize classes into namespaces
 package com.codingcossack.chatserver;
 
+// Import necessary classes from Java standard library
+import java.net.Socket;               // For communication with the server
+import java.io.IOException;           // For handling input/output exceptions
+import java.io.InputStream;           // For reading bytes from server socket
+import java.io.OutputStream;          // For writing bytes to server socket
+import java.io.BufferedReader;        // For reading text from a character-input stream
+import java.io.InputStreamReader;   // For converting bytes to characters
+import java.io.PrintWriter;           // For writing text to a character-output stream
+
 public class Client {
+    public static void main(String[] args) {
+        int portNumber = 8080;
+
+        try (Socket socket = new Socket("localhost", portNumber)) {
+            // Get the input stream associated with the socket, to receive data from the server.
+            InputStream inputStream = socket.getInputStream();
+            // Get the output stream associated with the socket, to send data to the server.
+            OutputStream outputStream = socket.getOutputStream();
+            // Create a BufferedReader to read text from the input stream.
+            // InputStreamReader converts bytes from input stream to characters.
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            // Create a PrintWriter to write text to the output stream.
+            // 'true' indicates it should auto flush after each write operation.
+            PrintWriter writer = new PrintWriter(outputStream, true);
+
+            // Create a BufferedReader to read input from the console (keyboard)
+            BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+
+            while (true) {
+                System.out.print("Enter a message to send to the server: ");
+                // Read a message from the user's input (console).
+                String message = consoleReader.readLine();
+                // Break the loop if the user wants to quit. For example, if the message is "quit".
+                if ("quit".equalsIgnoreCase(message)) {
+                    break;
+                }
+                // Send the message to the server.
+                writer.println(message);
+                // 3. Read the server's response and print it.
+                String serverResponse = reader.readLine();
+                System.out.println("Server responded: " + serverResponse);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/com/codingcossack/chatserver/Server.java
+++ src/com/codingcossack/chatserver/Server.java
@@ -2,54 +2,75 @@
 package com.codingcossack.chatserver;
 
 // Import necessary classes from Java standard library
-import java.net.ServerSocket;  // For listening for client connections
-import java.io.IOException;     // For handling input/output exceptions
-import java.io.InputStream;     // For reading bytes from client socket
-import java.io.OutputStream;    // For writing bytes to client socket
-import java.net.Socket;         // For communication between server and client
-import java.io.BufferedReader;  // For reading text from a character-input stream
-import java.io.InputStreamReader; // For converting bytes to characters
-import java.io.PrintWriter;     // For writing text to a character-output stream
+import java.net.ServerSocket;          // For listening for client connections
+import java.io.IOException;             // For handling input/output exceptions
+import java.io.InputStream;             // For reading bytes from client socket
+import java.io.OutputStream;            // For writing bytes to client socket
+import java.net.Socket;                 // For communication between server and client
+import java.io.BufferedReader;          // For reading text from a character-input stream
+import java.io.InputStreamReader;       // For converting bytes to characters
+import java.io.PrintWriter;             // For writing text to a character-output stream
 
-// Define the Server class
 public class Server {
 
-    // Define the main method as the entry point of the application
     public static void main(String[] args) {
-
-        // Define the port number on which the server will listen for client connections
+        // Define the port number on which the server will listen for connections
         int portNumber = 8080;
 
-        // Try to create a server socket and listen for client connections
+        // Try-with-resources block to auto-close resources and handle exceptions
         try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
-
-            // Display that the server has started and is listening on the specified port
             System.out.println("Server started and listening on Port " + portNumber);
 
-            // Infinite loop to keep the server running and accepting client connections
+            // Infinite loop to keep server running and accepting client connections
             while (true) {
-
-                // Accept a new client connection and obtain the socket for communication
+                // Wait for a client to connect, and obtain the socket representing the client connection
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("A new client is connected: " + clientSocket);
 
-                // Obtain the raw input stream from the client socket
+                // Obtain the input stream to read data sent by the client
                 InputStream inputStream = clientSocket.getInputStream();
 
-                // Obtain the raw output stream to the client socket
+                // Obtain the output stream to send data to the client
                 OutputStream outputStream = clientSocket.getOutputStream();
 
-                // Wrap the raw input stream with BufferedReader for efficient reading of text data
+                // Wrap the input stream with a BufferedReader to easily read text lines from the client
                 BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
-                // Wrap the raw output stream with PrintWriter for convenient writing of text data.
-                // 'true' argument indicates that PrintWriter should auto-flush the stream.
+                // Wrap the output stream with a PrintWriter, auto-flushing enabled, to easily write text lines to the client
                 PrintWriter writer = new PrintWriter(outputStream, true);
 
-            }
+                // Variable to store messages received from the client
+                String message;
 
+                // Infinite loop to continually read messages from the client
+                while (true) {
+                    // Read a line of text from the client
+                    message = reader.readLine();  // This could be null if the client has disconnected
+
+                    // If the message is null, the client has likely disconnected, so exit the loop
+                    if (message == null) {
+                        break;
+                    }
+
+                    // Output the received message to the server's console
+                    System.out.println("Received from client: " + message);
+
+                    // Send a response back to the client
+                    writer.println("Server received: " + message);
+
+                    // If the client sends the text "quit", exit the loop
+                    if ("quit".equalsIgnoreCase(message)) {
+                        break;
+                    }
+                }
+
+                // Close the client socket
+                clientSocket.close();
+                System.out.println("Client disconnected");
+
+            }
         } catch (IOException e) {
-            // Handle any IOException that might occur during network communication
+            // Output any IO exceptions that occur
             e.printStackTrace();
         }
     }
