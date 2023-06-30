210b11c0d36eefc679862a0832b8238391d46517
Dima <dimastasy@protonmail.com>
2023-06-30 17:47:04 +0100

    feat(Server): Add logging capability
    Incorporated SLF4J logging in the Server class. Logging is now used to inform of server start-up and shut-down events, successful user registrations, as well as socket connection and disconnection. This will provide valuable information on server-side activity and facilitate troubleshooting in the case of any unexpected server behaviors.

--- src/main/java/com/codingcossack/chatserver/Server.java
+++ src/main/java/com/codingcossack/chatserver/Server.java
@@ -6,6 +6,8 @@ import java.net.ServerSocket;          // For listening for client connections
 import java.io.IOException;             // For handling input/output exceptions
 import java.net.Socket;                 // For communication between server and client
 import java.util.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Server {
 
@@ -13,6 +15,8 @@ public class Server {
     static Map<String, String> userCredentials = new HashMap<>();
     // A synchronized list to store connected clients
     static final List<ClientHandler> clients = Collections.synchronizedList(new ArrayList<>());
+    // Initialize the logger
+    private static final Logger LOGGER = LoggerFactory.getLogger(Server.class);
 
     static {
         userCredentials.put("user1", "password1");
@@ -25,13 +29,13 @@ public class Server {
 
         // Try-with-resources block to auto-close resources and handle exceptions
         try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
-            System.out.println("Server started and listening on Port " + portNumber);
+            LOGGER.info("Server started and listening on Port {}", portNumber);
 
             // Infinite loop to keep server running and accepting client connections
             while (true) {
                 // Wait for a client to connect, and obtain the socket representing the client connection
                 Socket clientSocket = serverSocket.accept();
-                System.out.println("A new client is connected: " + clientSocket);
+                LOGGER.info("A new client is connected: {}", clientSocket);
 
                 ClientHandler clientHandler = new ClientHandler(clientSocket);
 
